9c8a6b8905cb5af88b2e8f1059bd5fbf226400cd
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-08-26 06:16:56 +0300

    introduced aggregation and group by

--- data.sql
+++ data.sql
@@ -5,6 +5,12 @@ INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (1,'Agumon', 'feb-3-2020',0,false,10.23);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (2,'Gabumon', 'nov-15-2018',2,true, 8);
-INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (3,'Pikachu', 'jan-7-2021,',1,true, 15.04)
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (3,'Pikachu', 'jan-7-2021,',1,true, 15.04);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (4,'Devimon', 'may-12-2017',5,true, 11);
-
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (5,'Charmander','feb-8-2020',0,false,11),
+(6,'Plantmon','nov-15-2021',2,true,5.7),
+(7,'Squirtle','apr-2-1993',3,false,12.13),
+(8,'Angemon','jun-12-2005,',1,true,45),
+(9,'Boarmon', 'jun-7-2005',7,true,20.4),
+(10,'Blossom', 'oct-13-1998',3,true,17),
+(11,'Ditto', 'may-14-2022,',4,true,22);
--- queries.sql
+++ queries.sql
@@ -9,3 +9,19 @@ SELECT escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg > 10.4 OR weight_kg < 17.3 OR weight_kg = 10.4 AND  weight_kg = 17.3;
+ALTER TABLE animals RENAME COLUMN species TO unspecified;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+DELETE FROM animals;
+DELETE FROM animals WHERE EXTRACT(MONTH FROM date_of_birth) > 1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT COUNT(*) FROM animals;
+SELECT * FROM animals WHERE escape_attempts = 0;
+SELECT * FROM animals AVG(weight_kg);
+SELECT neutered, AVG(escape_attempts) AS average_escape_count
+FROM animals
+GROUP BY neutered;
+SELECT * FROM animals MAX(weight_kg);
+SELECT * FROM animals MIN(height_kg);
+SELECT * FROM animals MIN(escape_attempts) WHERE EXTRACT(YEAR FROM date_of_birth) > 1990 AND EXTRACT(YEAR FROM date_of_birth) < 2000;
